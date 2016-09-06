946482b7e6fbec47042ed40d4538e9c30a979c91
Allister Banks <arubdesu@me.com>
2016-09-06 15:38:59 -0400

    fix the thing

--- Office2016-IndividualApps/MSOffice2016URLandUpdateInfoProvider.py
+++ Office2016-IndividualApps/MSOffice2016URLandUpdateInfoProvider.py
@@ -28,9 +28,9 @@ from autopkglib import Processor, ProcessorError
 __all__ = ["MSOffice2016URLandUpdateInfoProvider"]
 
 # CULTURE_CODE defaulting to 'en-US' as the installers and updates seem to be
-# multilingual.
+# multilingual. Only Production (vs. Insider Slow/Fast) option available
 CULTURE_CODE = "0409"
-BASE_URL = "https://officecdn.microsoft.com/pr/%s/OfficeMac/%s15.xml"
+BASE_URL = "https://officecdn.microsoft.com/pr/C1297A47-86C4-4C1F-97FA-950631F94777/OfficeMac/%s15.xml"
 PROD_DICT = {
     'Excel':'XCEL',
     'OneNote':'ONMC',
